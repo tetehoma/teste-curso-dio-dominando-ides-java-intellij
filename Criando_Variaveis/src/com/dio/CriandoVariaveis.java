6d042065210d09d3516ad76b8537683d2d2ba056
tetehoma <tetehoma@gmail.com>
2022-07-12 13:37:23 -0300

    Testando variaveis e metodos java

---
+++ Criando_Variaveis/src/com/dio/CriandoVariaveis.java
@@ -0,0 +1,57 @@
+package com.dio;
+
+public class CriandoVariaveis {
+    public static void main(String[] args) {
+        //int 1a - não é uma boa prática
+        //int qtdprod;
+        int i = 10;
+        int I = 100;
+        //Tipos de dados
+
+        byte b1 = 10;
+        byte b2 = 20;
+        short s1 = 20000;
+        //short s2 = 42000;
+
+        //int i1 = -1000000000000000000;
+        int i2 = 24503;
+        long I1 = 10000000000000000L;
+        //long L2 = 10000000000000000000;
+
+        //float f1 = 4.3;
+        float f2 = 0.453F;
+
+        double d1 = 41.56;
+        double d2 = 99.455D;
+
+        char c1 = 'w';
+        //char c2 = 'ww';
+
+        String st1 = "fulano";
+        String st2 = "Ciclano";
+        String st3 = "O carro saiu";
+
+        String st4 = "04/07/2021";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(i);
+        System.out.println(i2);
+        System.out.println(I);
+        System.out.println(I1);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(st4);
+        System.out.println(bo1);
+        System.out.println(bo2);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+    }
+}
