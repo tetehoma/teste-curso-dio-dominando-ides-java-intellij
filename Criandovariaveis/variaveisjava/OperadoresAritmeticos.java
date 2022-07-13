0946fc14bb89f760eddfea018366e86711df6ee1
tetehoma <tetehoma@gmail.com>
2022-07-13 06:57:20 -0300

    origin

---
+++ Criandovariaveis/variaveisjava/OperadoresAritmeticos.java
@@ -0,0 +1,23 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args){
+        System.out.println(a);
+
+    }
+    private static void prePos (){
+        int k = 10;
+
+        int i = ++k;
+        int j = k--;
+        int x = k;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+    private static void aritmetico(){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = 50;
+    }
+}
